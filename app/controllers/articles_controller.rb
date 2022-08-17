01deb133e3b715aa89fa26df22b29782589f5d48
forest-hide <ksmc.xx@gmail.com>
2022-08-18 04:25:21 +0900

    CRUD処理に必要なedit,update,destroyを追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show ]
+  before_action :set_article, only: %i[ show edit update destroy ]
 
   # GET /articles
   def index
@@ -13,6 +13,10 @@ class ArticlesController < ApplicationController
   # GET /articles/new
   def new
     @article = Article.new
+  end
+
+  # GET /articles/1/edit
+  def edit
   end
 
   # POST /articles
@@ -31,6 +35,22 @@ class ArticlesController < ApplicationController
     #   @article.save!
     #   render :show
     # end
+
+   # PATCH/PUT /articles/1
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: "Article was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /articles/1
+  def destroy
+    @article.destroy
+    edirect_to articles_url, notice: "Article was successfully destroyed."
+  end
+
 
   private
     def set_article
